2fc41b294ca5d2e8f88de6146624468d6ea298af
weisenlin <1209867203@qq.com>
2018-04-17 19:54:35 +0800

    kkk

---
+++ src/main/java/com/wsl/demo/model/Module.java
@@ -0,0 +1,83 @@
+package com.wsl.demo.model;
+
+public class Module {
+    private String moduleid;
+
+    private Integer level;
+
+    private String parentmoduleid;
+
+    private String modulename;
+
+    private String moduledesc;
+
+    private String url;
+
+    private String stopflag;
+
+    private Integer displayseq;
+
+    public String getModuleid() {
+        return moduleid;
+    }
+
+    public void setModuleid(String moduleid) {
+        this.moduleid = moduleid == null ? null : moduleid.trim();
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public String getParentmoduleid() {
+        return parentmoduleid;
+    }
+
+    public void setParentmoduleid(String parentmoduleid) {
+        this.parentmoduleid = parentmoduleid == null ? null : parentmoduleid.trim();
+    }
+
+    public String getModulename() {
+        return modulename;
+    }
+
+    public void setModulename(String modulename) {
+        this.modulename = modulename == null ? null : modulename.trim();
+    }
+
+    public String getModuledesc() {
+        return moduledesc;
+    }
+
+    public void setModuledesc(String moduledesc) {
+        this.moduledesc = moduledesc == null ? null : moduledesc.trim();
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url == null ? null : url.trim();
+    }
+
+    public String getStopflag() {
+        return stopflag;
+    }
+
+    public void setStopflag(String stopflag) {
+        this.stopflag = stopflag == null ? null : stopflag.trim();
+    }
+
+    public Integer getDisplayseq() {
+        return displayseq;
+    }
+
+    public void setDisplayseq(Integer displayseq) {
+        this.displayseq = displayseq;
+    }
+}
---
+++ src/main/java/com/wsl/demo/model/Operation.java
@@ -0,0 +1,103 @@
+package com.wsl.demo.model;
+
+public class Operation {
+    private String oprid;
+
+    private String moduleid;
+
+    private String oprname;
+
+    private String oprdesc;
+
+    private String stopflag;
+
+    private Integer displayseq;
+
+    private String agentunitflag;
+
+    private String ownerunitflag;
+
+    private String platunitflag;
+
+    private String flowflag;
+
+    public String getOprid() {
+        return oprid;
+    }
+
+    public void setOprid(String oprid) {
+        this.oprid = oprid == null ? null : oprid.trim();
+    }
+
+    public String getModuleid() {
+        return moduleid;
+    }
+
+    public void setModuleid(String moduleid) {
+        this.moduleid = moduleid == null ? null : moduleid.trim();
+    }
+
+    public String getOprname() {
+        return oprname;
+    }
+
+    public void setOprname(String oprname) {
+        this.oprname = oprname == null ? null : oprname.trim();
+    }
+
+    public String getOprdesc() {
+        return oprdesc;
+    }
+
+    public void setOprdesc(String oprdesc) {
+        this.oprdesc = oprdesc == null ? null : oprdesc.trim();
+    }
+
+    public String getStopflag() {
+        return stopflag;
+    }
+
+    public void setStopflag(String stopflag) {
+        this.stopflag = stopflag == null ? null : stopflag.trim();
+    }
+
+    public Integer getDisplayseq() {
+        return displayseq;
+    }
+
+    public void setDisplayseq(Integer displayseq) {
+        this.displayseq = displayseq;
+    }
+
+    public String getAgentunitflag() {
+        return agentunitflag;
+    }
+
+    public void setAgentunitflag(String agentunitflag) {
+        this.agentunitflag = agentunitflag == null ? null : agentunitflag.trim();
+    }
+
+    public String getOwnerunitflag() {
+        return ownerunitflag;
+    }
+
+    public void setOwnerunitflag(String ownerunitflag) {
+        this.ownerunitflag = ownerunitflag == null ? null : ownerunitflag.trim();
+    }
+
+    public String getPlatunitflag() {
+        return platunitflag;
+    }
+
+    public void setPlatunitflag(String platunitflag) {
+        this.platunitflag = platunitflag == null ? null : platunitflag.trim();
+    }
+
+    public String getFlowflag() {
+        return flowflag;
+    }
+
+    public void setFlowflag(String flowflag) {
+        this.flowflag = flowflag == null ? null : flowflag.trim();
+    }
+}
