12395e4720a1d523bc604ce383bae49ad0a8fb64
weisenlin <1209867203@qq.com>
2018-04-17 13:27:00 +0800

    整合freemaker

--- src/main/java/com/wsl/demo/controller/UserController.java
+++ src/main/java/com/wsl/demo/controller/UserController.java
@@ -6,15 +6,17 @@ import com.wsl.demo.model.Result;
 import com.wsl.demo.model.User;
 import com.wsl.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 名称:
  * 用途：
  * Created by SILVA_WSL on 2018/4/17 0017.
  */
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController {
     @Autowired
@@ -22,6 +24,7 @@ public class UserController {
 
 
     @RequestMapping("/list.do")
+    @ResponseBody
     public Result list(){
         Result result = new Result();
 //        List<User> users = userService.getAll();
@@ -30,5 +33,11 @@ public class UserController {
         PageInfo users = new PageInfo(pages);
         result.setData(users);
         return result;
+    }
+
+    @RequestMapping("/ftl.do")
+    public String testFtl(ModelMap mv){
+        mv.addAttribute("user",userService.getEntity("1"));
+        return "index";
     }
 }
---
+++ src/main/resources/templates/index.ftl
@@ -0,0 +1,3 @@
+ftl
+用户信息：
+        用户名:${user.username}
