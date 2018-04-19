4f1bb667138c5dcab7faba552b5465d94679b2ba
weisenlin <1209867203@qq.com>
2018-04-19 08:44:26 +0800

    单元测试

--- src/test/java/com/wsl/demo2/Demo2ApplicationTests.java
+++ src/test/java/com/wsl/demo2/Demo2ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.wsl.demo2;
 
+import com.wsl.demo.Demo2Application;
 import com.wsl.demo.model.User;
 import com.wsl.demo.service.UserService;
 import org.junit.Test;
@@ -9,12 +10,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = Demo2Application.class)
 public class Demo2ApplicationTests {
 	@Autowired
 	private UserService userService;
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testService(){
+		User user = userService.getEntity("1");
+		System.out.println(user.getUsername());
 	}
 
 }
